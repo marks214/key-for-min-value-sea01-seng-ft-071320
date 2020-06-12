81709630a4d5245b63a46a1f914506cfb0473a6f
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-12 01:19:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  minimum = 10000000
+  values = []
+  name_hash.each do |key, value|
+    if value < minimum
+      minimum = value
+      values.push(value, key)
+    end
+  end
+    return values[-1]
 end
